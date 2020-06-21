4d4b47506dc0f81e5f2fe55d6bae67294854220b
Fernando Augusto Medeiros Silva <fernando.augusto@gmail.com>
2020-06-21 04:34:31 -0300

    sem interpolacao

--- main.tf
+++ main.tf
@@ -238,7 +238,7 @@ resource "aws_iam_role_policy" "ecs_task_role_policy" {
 resource "aws_iam_role" "ecs_task_role" {
   name_prefix = "ecs_task_role"
   # path               = "/system/"
-  assume_role_policy = "${data.aws_iam_policy_document.ecs_task_assume_role_policy.json}"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role_policy.json
   tags               = var.tags
 }
 
